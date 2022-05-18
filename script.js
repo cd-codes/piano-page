90d7c78dcec58154e8814f4e3735c2e12bdc765d
Camille Dent <camilledent995@gmail.com>
2022-05-18 18:49:05 -0400

    Add arrays for chords and implement test function for tutorial

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 const keys = document.querySelectorAll('.key');
-const SONG_NOTES = ["d3", "g3", "a-4", "d4", "d4", "c4", "a-4", "a4", "a-4",
-     "g3", "a-4", "d4", "g4", "g4", "g4", "f4", "d-4", "f4",
-     "a4", "d4", "f4", "a5", "g4", "f4", "e4", "f4", "g4", "f4", "e4",
-     "d4", "c4", "a-4", "c4", "d4", "c4", "g3", "a4"];
+// const SONG_NOTES = ["d3", "g3", "a-4", "d4", "d4", "c4", "a-4", "a4", "a-4",
+//      "g3", "a-4", "d4", "g4", "g4", "g4", "f4", "d-4", "f4",
+//      "a4", "d4", "f4", "a5", "g4", "f4", "e4", "f4", "g4", "f4", "e4",
+//      "d4", "c4", "a-4", "c4", "d4", "c4", "g3", "a4"];
+const cMajor = ["c3", "e3", "g3", "c4"];
+const gMajor = ["g3", "b4", "d4", "g4"];
+const dMajor = ["d3", "f-3", "a4", "d4"];
+const aMajor = ["a4", "c-4", "e4", "a5"];
+const eMajor = ["e3", "g-3", "b4", "e4"];
+const bMajor = ["b4", "d-4", "f-4", "b5"];
+const fsMajor = ["f-3", "a-4", "c-4", "f-4"];
+const csMajor = ["c-3", "f3", "g-3", "c-4"];
+const afMajor = ["g-3", "c4", "d-4", "g-4"];
+const efMajor = ["d-3", "g3", "a-4", "d-4"];
+const bfMajor = ["a-4", "d4", "f4", "a-5"];
+const fMajor = ["f3", "a4", "c4", "f4"];
 
 keys.forEach(key => {
     key.addEventListener('click', () => playNote(key));
@@ -18,11 +30,26 @@ function playNote(key) {
     })
 }
 
-/*
-user clicks the start song button
+function chordTest(chord) {
+    chord.forEach(note => {
+        keys.forEach(key => {
+            if(key.dataset.note == note) {
+                key.classList.add("note-highlight");
+                key.addEventListener('click', () => {
+                    key.classList.remove("note-highlight");
+                })
+            }
+        })
+    })
+}
 
-for each song note,
-turn the highlight on,
-turn the highligh off when the note is clicked,
-go to the next song note
+chordTest(fMajor);
+
+/*
+user clicks on a chord button
+the relevant keys highlight in green
+as the user clicks a key, the highlight goes away
+the user can click the reset keys button at the top to turn off all highlights
+the user can click the same chord button again to rehighlight all the keys
+the user can click any other chord button and the highlighted keys will switch to the keys for that chord
 */
