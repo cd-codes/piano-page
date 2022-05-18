7943640811d3d3d21c7d3e48c8b9f18bc57bdccf
Camille Dent <camilledent995@gmail.com>
2022-05-18 09:33:21 -0400

    Add song note array to script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const keys = document.querySelectorAll('.key');
+const SONG_NOTES = ["d3", "g3", "a-4", "d4", "d4", "c4", "a-4", "a4", "a-4",
+     "g3", "a-4", "d4", "g4", "g4", "g4", "f4", "d-4", "f4",
+     "a4", "d4", "f4", "a5", "g4", "f4", "e4", "f4", "g4", "f4", "e4",
+     "d4", "c4", "a-4", "c4", "d4", "c4", "g3", "a4"];
 
 keys.forEach(key => {
     key.addEventListener('click', () => playNote(key));
